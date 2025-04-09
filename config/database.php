11f48143c489a3039047f7fd816b49ba72c33ac6
Jessica Smith <jessica.smith@digicert.com>
2025-04-09 13:59:59 +0100

    Update redis config to allow specifying schema and clusters

--- config/database.php
+++ config/database.php
@@ -151,7 +151,22 @@ return [
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
+        'clusters' => [
+            'default' => [
+                [
+                    'scheme' => env('REDIS_SCHEME', 'tcp'),
+                    'url' => env('REDIS_URL'),
+                    'host' => env('REDIS_HOST', '127.0.0.1'),
+                    'username' => env('REDIS_USERNAME'),
+                    'password' => env('REDIS_PASSWORD'),
+                    'port' => env('REDIS_PORT', '6379'),
+                    'database' => env('REDIS_DB', '0'),
+                ],
+            ],
+        ],
+
         'default' => [
+            'scheme' => env('REDIS_SCHEME', 'tcp'),
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
